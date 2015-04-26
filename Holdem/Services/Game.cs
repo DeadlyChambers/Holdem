f8787f6a7f1f1990ed6607d947050f903815241f
Shane Chambers <schambers@envisiontechnology.com>
2015-04-26 06:06:05 -0600

    adding game

---
+++ Holdem/Services/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Holdem.Services
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public virtual List<Player> Players
+        {
+            get; set; }
+        public virtual List<Table> Tables { get; set; } 
+
+    }
+}
